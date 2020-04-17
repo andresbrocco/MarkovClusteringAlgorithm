d276df02b83e05e922a6d1274a51db8b1a83c522
andresbrocco <andresbrocco@github.com>
2020-04-17 16:25:36 -0300

    started to draw on canvas

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,18 @@
-let graphNodes = [];
-let value = 155;
+let nodes = [];
+let nodeRadius = 5;
+let clickedNode = [];
+let edges = [];
 
 function setup() {
   var canvas = createCanvas(parseFloat(select('#sketch-holder').style('width')),parseFloat( select('#sketch-holder').style('height')));
   canvas.parent('sketch-holder');
   console.log("sketch-holder width: "+select('#sketch-holder').style('width')+ "  height: "+select('#sketch-holder').style('height'))
+  background(color('hsl(180, 37%, 79%)'));
 }
 
 function draw() {
-  background(color('hsl(180, 37%, 79%)'));
+  drawNodes();
+  drawEdges();
 }
 
 function windowResized() {
@@ -18,10 +22,46 @@ function windowResized() {
 
 function mousePressed() {
   if (mouseX <= width && mouseY <= height){
-    if (value === 0) {
-      value = 155;
-    } else {
-      value = 0;
+    clickedNode = getNode(mouseX, mouseY);
+  }
+}
+
+function mouseDragged() {
+  if (dist(clickedNode.posX, clickedNode.posY, mouseX, mouseY) > 2*nodeRadius) {
+    line(clickedNode.posX, clickedNode.posY, mouseX, mouseY);
+  }
+}
+
+function getNode(mouseX, mouseY) {
+  for (var node = 0; node < nodes.length; node++) {
+    if (dist(nodes[node].posX, nodes[node].posY, mouseX, mouseY) < nodeRadius) {
+      console.log("You clicked on the node: " + node);
+      return nodes[node];
     }
   }
+  console.log("You created a new node: " + node)
+  var newNode = createNode(mouseX, mouseY);
+  return newNode;
+}
+
+function createNode(mouseX, mouseY) {
+  nodes.push({
+    posX:mouseX,
+    posY:mouseY,
+    draw: function() {
+      fill('rgb(14, 32, 32)');
+      circle(this.posX, this.posY, 2*nodeRadius);
+    }
+  });
+  return nodes[nodes.length - 1]
+}
+
+function drawNodes() {
+  for (var node = 0; node < nodes.length; node++) {
+    nodes[node].draw();
+  }
+}
+
+function drawEdges() {
+
 }
