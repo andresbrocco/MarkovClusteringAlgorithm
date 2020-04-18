aa1310b947575f802b5d9417e815f9a813a59744
andresbrocco <andresbrocco@github.com>
2020-04-18 02:59:53 -0300

    node deletion is working

--- js/sketch.js
+++ js/sketch.js
@@ -38,8 +38,8 @@ function mouseReleased() {
   if (mouseIsOnCanvas()) {
     releasedNode = getNode(mouseX, mouseY);
     if (pressedNode != releasedNode) { // CHANGE THIS WHEN DIRECTED/UNDIRECTED IS IMPLEMENTED!
-      edges.set([pressedNode.id, releasedNode.id], 1);
-      edges.set([releasedNode.id, pressedNode.id], 1);
+      edges.set([pressedNode, releasedNode], 1);
+      edges.set([releasedNode, pressedNode], 1);
     } else if (isDeletingNode) {
       deleteNode(pressedNode);
     }
@@ -54,26 +54,25 @@ function mouseReleased() {
 function mouseDragged() {
   if (mouseIsOnCanvas()) {
     if (isMovingNode) {
-      pressedNode.posX = mouseX;
-      pressedNode.posY = mouseY;
+      nodes[pressedNode].posX = mouseX;
+      nodes[pressedNode].posY = mouseY;
     }
   }
 }
 
 function getNode(mouseX, mouseY) {
   for (var node = 0; node < nodes.length; node++) { // Try to find node where mouse clicked:
-    if (dist(nodes[node].posX, nodes[node].posY, mouseX, mouseY) < 4*nodeRadius) { // Deny nodes too close to each other
-      return nodes[node];
+    if (dist(nodes[node].posX, nodes[node].posY, mouseX, mouseY) < 4*nodeRadius) { // Avoid creating nodes too close to each other
+      return node;
     }
   }
   // If didn't find node, create one:
-  var newNode = createNode(mouseX, mouseY);
-  return newNode;
+  createNode(mouseX, mouseY);
+  return nodes.length-1;
 }
 
 function createNode(mouseX, mouseY) {
   nodes.push({
-    id:nodes.length,
     posX:mouseX,
     posY:mouseY,
     draw: function() {
@@ -83,25 +82,27 @@ function createNode(mouseX, mouseY) {
   });
   // Increase edge matrix size:
   edges = math.resize(edges, [nodes.length, nodes.length], 0);
-  return nodes[nodes.length - 1]
 }
 
 function deleteNode(node) {
-  console.log("original edges: "+edges);
-  // Remove row:
-  console.log("keep ranges: "+math.index(math.range(0, nodes.length), math.range(0, node.id)) + " and " + math.index(math.range(0, nodes.length), math.range(node.id+1, nodes.length)));
-  console.log("first range: " +edges.subset(math.index(math.range(0, nodes.length), math.range(0,         node.id))));
-  console.log("second range: "+edges.subset(math.index(math.range(0, nodes.length), math.range(node.id+1, nodes.length))));
-
-  edges = math.concat(edges.subset(math.index(math.range(0, nodes.length), math.range(0,         node.id))),
-                      edges.subset(math.index(math.range(0, nodes.length), math.range(node.id+1, nodes.length))), 1);
-  console.log("edges without row: "+edges);
-  // Remove column:
-  edges = math.concat(edges.subset(math.index(math.range(0,         node.id),      math.range(0, nodes.length-1))),
-                      edges.subset(math.index(math.range(node.id+1, nodes.length), math.range(0, nodes.length-1))), 0);
-  console.log("edges without column: "+edges);
+  if(nodes.length > 1){
+    if(node === 0) {
+      edges = edges.subset(math.index(math.range(1, nodes.length), math.range(1, nodes.length)));
+    } else if (node === nodes.length-1) {
+      edges = edges.subset(math.index(math.range(0, nodes.length-1), math.range(0, nodes.length-1)));
+    } else {
+      // Remove row:
+      edges = math.concat(edges.subset(math.index(math.range(0, nodes.length), math.range(0,         node))),
+                          edges.subset(math.index(math.range(0, nodes.length), math.range(node+1, nodes.length))), 1);
+      // Remove column:
+      edges = math.concat(edges.subset(math.index(math.range(0,         node),      math.range(0, nodes.length-1))),
+                          edges.subset(math.index(math.range(node+1, nodes.length), math.range(0, nodes.length-1))), 0);
+    }
+  } else {
+    edges = math.matrix([0])
+  }
   // Remove node:
-  nodes.splice(node.id, 1);
+  nodes.splice(node, 1);
 }
 
 function drawNodes() {
@@ -113,18 +114,20 @@ function drawNodes() {
 function drawEdges() {
   // Draw edge that is being connected:
   if(isMakingNewEdge){
-    if (dist(pressedNode.posX, pressedNode.posY, mouseX, mouseY) > 2*nodeRadius) {
-      line(pressedNode.posX, pressedNode.posY, mouseX, mouseY);
+    if (dist(nodes[pressedNode].posX, nodes[pressedNode].posY, mouseX, mouseY) > 2*nodeRadius) {
+      line(nodes[pressedNode].posX, nodes[pressedNode].posY, mouseX, mouseY);
     }
   }
   // Draw the already connected edges:
-  edges.forEach(
-    function (value, index, matrix) {
-      if (value != 0) {
-        line(nodes[index[0]].posX, nodes[index[0]].posY, nodes[index[1]].posX, nodes[index[1]].posY);
+  if(nodes.length > 1) {
+    edges.forEach(
+      function (value, index, matrix) {
+        if (value != 0) {
+          line(nodes[index[0]].posX, nodes[index[0]].posY, nodes[index[1]].posX, nodes[index[1]].posY);
+        }
       }
-    }
-  );
+    );
+  }
 }
 
 function mouseIsOnCanvas() {
