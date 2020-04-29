441a9e91fdf2ce13dcf4d0818bb740d7db90bb1e
Andre Sbrocco <andresbrocco@gmail.com>
2020-04-29 20:35:02 -0300

    fixed node deletion and added segregation method

--- js/sketch.js
+++ js/sketch.js
@@ -7,17 +7,18 @@ let isMovingNode = false;
 let isDeletingNode = false;
 let edges = math.matrix([0]);
 let clusteredEdges;
+let canonicalTransitionMatrix;
 let clusteringBegan = false;
-let graphMovement = 'fixed';
+let graphMovement = 'fixed'; // 'fixed' 'relaxed'
 let inflationValue = 2;
 let pruneTresholdValue = 1;
 let clusterizationSpeedValue = 1;
 let clusteringRunning = false;
-let nodeToNodeRepulsionFactor = 0.9;
+let nodeToNodeRepulsionFactor = 0.6;
 let nodeToNodeAttractionFactor = 1;
 let nodeToCenterAttractionFactorX = 0;
 let nodeToCenterAttractionFactorY = 0;
-let dampingFactor = 0.1;
+let dampingFactor = 0.15;
 let nodesVelocity = math.matrix();
 let randomForce = math.matrix();
 let clusteringConverged = false;
@@ -26,6 +27,7 @@ let isRecording = false;
 let gif;
 let waitingForDownload = false;
 let targetFrameRate = 30;
+let segregationMethod = 'standard'; // 'standard' or 'regularized'
 
 math.DenseMatrix.prototype.broadcast = function () {
   let broadcasted;
@@ -146,6 +148,7 @@ function mousePressed() {
         isMovingNode = true;
       } else if (keyIsDown(68)) { // 68: keycode for "d"
         isDeletingNode = true;
+        deleteNode(pressedNodeId);
       } else { // No key pressed
         isMakingNewEdge = true;
       }
@@ -154,23 +157,21 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  if(!clusteringBegan && !waitingForDownload){
+  if(!clusteringBegan && !waitingForDownload && !isDeletingNode){
     if (mouseIsOnCanvas() && isMakingNewEdge) {
       releasedNodeId = getNodeId(mouseX, mouseY);
-      if (pressedNodeId != releasedNodeId) { // CHANGE THIS WHEN DIRECTED/UNDIRECTED IS IMPLEMENTED!
+      if (pressedNodeId != releasedNodeId) {
         edges.set([pressedNodeId, releasedNodeId], 1);
         edges.set([releasedNodeId, pressedNodeId], 1);
 
-      } else if (isDeletingNode) {
-        deleteNode(pressedNodeId);
       }
     }
-    pressedNodeId = [];
-    releasedNodeId = [];
-    isMovingNode = false;
-    isMakingNewEdge = false;
-    isDeletingNode = false;
   }
+  pressedNodeId = [];
+  releasedNodeId = [];
+  isMovingNode = false;
+  isMakingNewEdge = false;
+  isDeletingNode = false;
 }
 
 function mouseDragged() {
@@ -207,9 +208,9 @@ function getEdgesUnderCursor(mouseX, mouseY) {
     function (value, edgeNodesIds, matrix) {
       if(value > 0) { // if edge exists
         if(distToEdge(mouseX, mouseY, nodes.get([edgeNodesIds[0], 0])*width,
-                                      nodes.get([edgeNodesIds[0], 1])*height,
-                                      nodes.get([edgeNodesIds[1], 0])*width,
-                                      nodes.get([edgeNodesIds[1], 1])*height) < nodeRadius) {
+        nodes.get([edgeNodesIds[0], 1])*height,
+        nodes.get([edgeNodesIds[1], 0])*width,
+        nodes.get([edgeNodesIds[1], 1])*height) < nodeRadius) {
           edgesUnderCursor.push(edgeNodesIds);
         }
       }
@@ -221,8 +222,8 @@ function getEdgesUnderCursor(mouseX, mouseY) {
 function getNodeId(mouseX, mouseY) {
   for (var nodeId = 0; nodeId < nodes.size()[0]; nodeId++) { // Try to find node where mouse clicked:
     if (dist(nodes.get([nodeId, 0])*width,
-             nodes.get([nodeId, 1])*height,
-             mouseX, mouseY) < 4*nodeRadius) { // Avoid creating nodes too close to each other
+    nodes.get([nodeId, 1])*height,
+    mouseX, mouseY) < 4*nodeRadius) { // Avoid creating nodes too close to each other
       return nodeId;
     }
   }
@@ -254,16 +255,16 @@ function deleteNode(nodeId) {
     } else {
       // Remove row:
       edges = math.concat(edges.subset(math.index(math.range(0,         nodeId),  math.range(0, nOfNodes))),
-                          edges.subset(math.index(math.range(nodeId+1, nOfNodes), math.range(0, nOfNodes))), 0);
+      edges.subset(math.index(math.range(nodeId+1, nOfNodes), math.range(0, nOfNodes))), 0);
       nodes = math.concat(nodes.subset(math.index(math.range(0, nodeId), [0, 1])),
-                          nodes.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
+      nodes.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
       nodesVelocity = math.concat(nodesVelocity.subset(math.index(math.range(0, nodeId), [0, 1])),
-                                  nodesVelocity.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
+      nodesVelocity.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
       randomForce = math.concat(randomForce.subset(math.index(math.range(0, nodeId), [0, 1])),
-                                randomForce.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
+      randomForce.subset(math.index(math.range(nodeId+1, nOfNodes), [0, 1])), 0);
       // Remove column:
       edges = math.concat(edges.subset(math.index(math.range(0, nOfNodes-1), math.range(0,         nodeId))),
-                          edges.subset(math.index(math.range(0, nOfNodes-1), math.range(nodeId+1, nOfNodes))), 1);
+      edges.subset(math.index(math.range(0, nOfNodes-1), math.range(nodeId+1, nOfNodes))), 1);
     }
   } else {
     edges = math.matrix([0]);
@@ -292,181 +293,187 @@ function drawNodes() {
           circle(nodes.get([index[0], 0])*width, nodes.get([index[0], 1])*height, 2*nodeRadius);
         }
       })
-  }
-}
-
-function drawEdges() {
-  // Draw edge that is being connected:
-  if(isMakingNewEdge){
-    if (dist(nodes.get([pressedNodeId, 0])*width, nodes.get([pressedNodeId, 1])*height, mouseX, mouseY) > 2*nodeRadius) {
-      stroke(color(50, 50, 50, 200));
-      strokeWeight(nodeRadius*0.75)
-      line(nodes.get([pressedNodeId, 0])*width, nodes.get([pressedNodeId, 1])*height, mouseX, mouseY);
     }
   }
-  // Draw the already connected edges:
-  if(nodes.size()[0] > 1) {
-    edges.forEach(
-      function (edgeWeight, edgeNodesIds, matrix) {
-        if (edgeWeight != 0) {
-          stroke(color(50, 50, 50, 128*edgeWeight));
-          strokeWeight(nodeRadius*edgeWeight*0.75)
-          line(nodes.get([edgeNodesIds[0], 0])*width,
-               nodes.get([edgeNodesIds[0], 1])*height,
-               nodes.get([edgeNodesIds[1], 0])*width,
-               nodes.get([edgeNodesIds[1], 1])*height);
-        }
+
+  function drawEdges() {
+    // Draw edge that is being connected:
+    if(isMakingNewEdge){
+      if (dist(nodes.get([pressedNodeId, 0])*width, nodes.get([pressedNodeId, 1])*height, mouseX, mouseY) > 2*nodeRadius) {
+        stroke(color(50, 50, 50, 200));
+        strokeWeight(nodeRadius*0.75)
+        line(nodes.get([pressedNodeId, 0])*width, nodes.get([pressedNodeId, 1])*height, mouseX, mouseY);
       }
-    );
-  }
-  // Draw clusteredEdges:
-  if(clusteringBegan) {
-    clusteredEdges.forEach(
-      function (edgeWeight, edgeNodesIds, matrix) {
-        if (edgeWeight != 0) {
-          stroke(color(255, 0, 0, 128*edgeWeight));
-          strokeWeight(nodeRadius*edgeWeight*0.75)
-          line(nodes.get([edgeNodesIds[0], 0])*width,
-               nodes.get([edgeNodesIds[0], 1])*height,
-               nodes.get([edgeNodesIds[1], 0])*width,
-               nodes.get([edgeNodesIds[1], 1])*height);
+    }
+    // Draw the already connected edges:
+    if(nodes.size()[0] > 1) {
+      edges.forEach(
+        function (edgeWeight, edgeNodesIds, matrix) {
+          if (edgeWeight != 0) {
+            stroke(color(50, 50, 50, 128*edgeWeight));
+            strokeWeight(nodeRadius*edgeWeight*0.75)
+            line(nodes.get([edgeNodesIds[0], 0])*width,
+            nodes.get([edgeNodesIds[0], 1])*height,
+            nodes.get([edgeNodesIds[1], 0])*width,
+            nodes.get([edgeNodesIds[1], 1])*height);
+          }
         }
-      }
-    );
+      );
+    }
+    // Draw clusteredEdges:
+    if(clusteringBegan) {
+      clusteredEdges.forEach(
+        function (edgeWeight, edgeNodesIds, matrix) {
+          if (edgeWeight != 0) {
+            stroke(color(255, 0, 0, 128*edgeWeight));
+            strokeWeight(nodeRadius*edgeWeight*0.75)
+            line(nodes.get([edgeNodesIds[0], 0])*width,
+            nodes.get([edgeNodesIds[0], 1])*height,
+            nodes.get([edgeNodesIds[1], 0])*width,
+            nodes.get([edgeNodesIds[1], 1])*height);
+          }
+        }
+      );
+    }
   }
-}
 
-function mouseIsOnCanvas() {
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    return true;
-  } else {
-    return false;
+  function mouseIsOnCanvas() {
+    if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+      return true;
+    } else {
+      return false;
+    }
   }
-}
 
-function setInflationValue(value) {
-  inflationValue = value;
-}
-
-function setPruneTresholdValue(value) {
-  pruneTresholdValue = value;
-}
-
-function setClusterizationSpeed(value) {
-  clusterizationSpeedValue = value;
-}
-
-function setGraphMovement(value) {
-  graphMovement = value;
-}
-
-function record() {
-  if(isRecording) {
-    isRecording = false;
-    blockUser();
-    gif.render(function(progressPercentage) {document.getElementById('gifProgressBar').style.width = (100*progressPercentage+"%")}); // Percentage is from 0 to 1;
-  } else {
-    gif = new GIF({workers:3, quality:10, workerScript:'./js/gif.worker.js'});
-    gif.on('finished', function(generatedGif) {unBlockUser(); window.open(URL.createObjectURL(generatedGif));});
-    isRecording = true;
+  function setInflationValue(value) {
+    inflationValue = value;
   }
-  updateRecButton();
-}
 
-function resetClusterization() {
-  clusteredEdges = [];
-  clusteringBegan = false;
-  clusteringRunning = false;
-  clusteringConverged = false;
-  clusterIterationNumber = 0;
-  updatePlayPauseButton();
-}
-
-function playPauseClusterization() {
-  if(!clusteringBegan) {
-    beginClustering();
+  function setPruneTresholdValue(value) {
+    pruneTresholdValue = value;
   }
-  clusteringRunning = !clusteringRunning;
-  updatePlayPauseButton();
-}
 
-function beginClustering() {
-  clusteredEdges = math.add(edges, math.identity(edges.size()[0]));
-  clusteredEdges = math.dotDivide(clusteredEdges, clusteredEdges.colSum().broadcast()) ; // Renormalize
-  clusteringBegan = true;
-  clusterIterationNumber = 0;
-}
+  function setClusterizationSpeed(value) {
+    clusterizationSpeedValue = value;
+  }
 
-function stepClustering() {
-  let newClusteredEdges = clusteredEdges;
-  newClusteredEdges = math.multiply(newClusteredEdges, newClusteredEdges); // Segregate
-  newClusteredEdges = math.dotPow(newClusteredEdges, inflationValue); // Inflate
-  newClusteredEdges = math.dotDivide(newClusteredEdges, newClusteredEdges.colSum().broadcast()) ; // Renormalize
-  let pruneTreshold = pruneTresholdValue/newClusteredEdges.size()[0];
-  newClusteredEdges = math.map(newClusteredEdges, function(value){ // Prune
-    if(value < pruneTreshold) return 0; else return value;
-  });
-  if(math.deepEqual(newClusteredEdges, clusteredEdges)) {
-    clusteringConverged = true;
+  function setGraphMovement(value) {
+    graphMovement = value;
+  }
+
+  function setSegregationMethod(value) {
+    segregationMethod = value;
+  }
+
+  function record() {
+    if(isRecording) {
+      isRecording = false;
+      blockUser();
+      gif.render(function(progressPercentage) {document.getElementById('gifProgressBar').style.width = (100*progressPercentage+"%")}); // Percentage is from 0 to 1;
+    } else {
+      gif = new GIF({workers:3, quality:10, workerScript:'./js/gif.worker.js'});
+      gif.on('finished', function(generatedGif) {unBlockUser(); window.open(URL.createObjectURL(generatedGif));});
+      isRecording = true;
+    }
+    updateRecButton();
+  }
+
+  function resetClusterization() {
+    clusteredEdges = [];
+    clusteringBegan = false;
     clusteringRunning = false;
+    clusteringConverged = false;
+    clusterIterationNumber = 0;
     updatePlayPauseButton();
-    console.log("clustering converged:");
-    logMatrix(newClusteredEdges);
   }
-  clusteredEdges = newClusteredEdges;
-  clusterIterationNumber++;
-}
 
-function distToEdge(px, py, e1x, e1y, e2x, e2y) {
-  var edgeLengthSquared = sq(e1x-e2x)+ sq(e1y-e2y);
-  if (edgeLengthSquared === 0) return dist(px, py, e1x, e1y); // Should never fall here, but anyway...
-  var t = ((px - e1x) * (e2x - e1x) + (py - e1y) * (e2y - e1y))/edgeLengthSquared;
-  t = constrain(t, 0, 1);
-  return dist(px, py, e1x + t*(e2x - e1x), e1y + t*(e2y - e1y));
-}
-
-function logMatrix(matrix) {
-  for (var row = 0; row < matrix.size()[0]; row++) {
-    console.log("row "+row+": "+math.row(matrix, row));
+  function playPauseClusterization() {
+    if(!clusteringBegan) {
+      beginClustering();
+    }
+    clusteringRunning = !clusteringRunning;
+    updatePlayPauseButton();
   }
-}
 
-function updatePlayPauseButton(){
-  if (clusteringRunning) {
-    $('#playPauseButton').html('<i class="fa fa-pause"></i>');
-  } else {
-    $('#playPauseButton').html('<i class="fa fa-play"></i>');
+  function beginClustering() {
+    canonicalTransitionMatrix = math.add(edges, math.identity(edges.size()[0]));
+    canonicalTransitionMatrix = math.dotDivide(canonicalTransitionMatrix, canonicalTransitionMatrix.colSum().broadcast()) ; // Renormalize
+    clusteredEdges = canonicalTransitionMatrix;
+    clusteringBegan = true;
+    clusterIterationNumber = 0;
   }
-}
 
-function updateRecButton(){
-  if (isRecording) {
-    document.getElementById("recButtonIcon").style.color = "red";
-    document.getElementById("recButton").classList.add("pulsate");
-  } else {
-    document.getElementById("recButtonIcon").style.color = "rgb(2, 97, 197)";
-    document.getElementById("recButton").classList.remove("pulsate");
+  function stepClustering() {
+    let newClusteredEdges = clusteredEdges;
+    if(segregationMethod == 'standard') {newClusteredEdges = math.multiply(newClusteredEdges, newClusteredEdges);} // Segregate
+    else if(segregationMethod == 'regularized') {newClusteredEdges = math.multiply(newClusteredEdges, canonicalTransitionMatrix);} // Segregate
+    newClusteredEdges = math.dotPow(newClusteredEdges, inflationValue); // Inflate
+    newClusteredEdges = math.dotDivide(newClusteredEdges, newClusteredEdges.colSum().broadcast()) ; // Renormalize
+    let pruneTreshold = pruneTresholdValue/newClusteredEdges.size()[0];
+    newClusteredEdges = math.map(newClusteredEdges, function(value){ // Prune
+      if(value < pruneTreshold) return 0; else return value;
+    });
+    if(math.deepEqual(newClusteredEdges, clusteredEdges)) {
+      clusteringConverged = true;
+      clusteringRunning = false;
+      updatePlayPauseButton();
+      console.log("clustering converged:");
+      logMatrix(newClusteredEdges);
+    }
+    clusteredEdges = newClusteredEdges;
+    clusterIterationNumber++;
   }
-}
 
-function blockUser() {
-  document.getElementById("blockUserOverlay").classList.add("showBlockUserOverlay");
-  document.getElementById("blockUserOverlay").classList.remove("hideBlockUserOverlay");
-  waitingForDownload = true;
-}
+  function distToEdge(px, py, e1x, e1y, e2x, e2y) {
+    var edgeLengthSquared = sq(e1x-e2x)+ sq(e1y-e2y);
+    if (edgeLengthSquared === 0) return dist(px, py, e1x, e1y); // Should never fall here, but anyway...
+    var t = ((px - e1x) * (e2x - e1x) + (py - e1y) * (e2y - e1y))/edgeLengthSquared;
+    t = constrain(t, 0, 1);
+    return dist(px, py, e1x + t*(e2x - e1x), e1y + t*(e2y - e1y));
+  }
 
-function unBlockUser() {
-  document.getElementById("blockUserOverlay").classList.add("hideBlockUserOverlay");
-  document.getElementById("blockUserOverlay").classList.remove("showBlockUserOverlay");
-  waitingForDownload = false;
-}
+  function logMatrix(matrix) {
+    for (var row = 0; row < matrix.size()[0]; row++) {
+      console.log("row "+row+": "+math.row(matrix, row));
+    }
+  }
 
-function writeInfoOnCanvas() {
-  noStroke();
-  fill(color(0, 0, 0));
-  text('Clustering iteration number: '+clusterIterationNumber+' '+
-        (clusteringConverged ? '(converged!)':'')+'\n'+
-       'Prune treshold: '+pruneTresholdValue+'\n'+
-       'Inflation: '+inflationValue,
-       0.03*width, 0.03*height);
-}
+  function updatePlayPauseButton(){
+    if (clusteringRunning) {
+      $('#playPauseButton').html('<i class="fa fa-pause"></i>');
+    } else {
+      $('#playPauseButton').html('<i class="fa fa-play"></i>');
+    }
+  }
+
+  function updateRecButton(){
+    if (isRecording) {
+      document.getElementById("recButtonIcon").style.color = "red";
+      document.getElementById("recButton").classList.add("pulsate");
+    } else {
+      document.getElementById("recButtonIcon").style.color = "rgb(2, 97, 197)";
+      document.getElementById("recButton").classList.remove("pulsate");
+    }
+  }
+
+  function blockUser() {
+    document.getElementById("blockUserOverlay").classList.add("showBlockUserOverlay");
+    document.getElementById("blockUserOverlay").classList.remove("hideBlockUserOverlay");
+    waitingForDownload = true;
+  }
+
+  function unBlockUser() {
+    document.getElementById("blockUserOverlay").classList.add("hideBlockUserOverlay");
+    document.getElementById("blockUserOverlay").classList.remove("showBlockUserOverlay");
+    waitingForDownload = false;
+  }
+
+  function writeInfoOnCanvas() {
+    noStroke();
+    fill(color(0, 0, 0));
+    text('Clustering iteration number: '+clusterIterationNumber+' '+
+    (clusteringConverged ? '(converged!)':'')+'\n'+
+    'Prune treshold: '+pruneTresholdValue+'\n'+
+    'Inflation: '+inflationValue,
+    0.03*width, 0.03*height);
+  }
